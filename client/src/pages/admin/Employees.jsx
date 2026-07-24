610a2921bf4e5280ad1689ba43ebdbd58195916d
Aryan Gupta <agaryangupta123@gmail.com>
2026-07-24 11:07:58 +0530

    responsive

--- client/src/pages/admin/Employees.jsx
+++ client/src/pages/admin/Employees.jsx
@@ -47,26 +47,26 @@ function Employees() {
             <br />
             <br />
 
-            <div className="flex justify-center">
+            <div className="w-full flex justify-center">
 
                 <div className="w-full max-w-6xl">
 
                     {/* Heading */}
 
-                    <div className="flex items-center justify-between mb-10">
+                    <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-6 mb-10">
 
                         <div>
 
                             <h1 className={`text-4xl font-bold transition-colors duration-300 ${theme === "dark"
-                                    ? "text-white"
-                                    : "text-slate-800"
+                                ? "text-white"
+                                : "text-slate-800"
                                 }`}>
                                 Employees
                             </h1>
 
                             <p className={`mt-3 text-lg transition-colors duration-300 ${theme === "dark"
-                                    ? "text-slate-400"
-                                    : "text-slate-500"
+                                ? "text-slate-400"
+                                : "text-slate-500"
                                 }`}>
                                 Manage all employees in the organization.
                             </p>
@@ -75,14 +75,15 @@ function Employees() {
 
                         {/* Total Employees */}
 
-                        <div className={`rounded-xl shadow-xl border px-6 py-5 min-w-47.5 text-center transition-colors duration-300 ${theme === "dark"
+                        <div
+                            className={`w-full lg:w-auto rounded-xl shadow-xl border px-6 py-5 text-center transition-colors duration-300 ${theme === "dark"
                                 ? "bg-slate-900 border-slate-700"
                                 : "bg-white border-slate-200"
-                            }`}>
+                                }`}>
 
                             <p className={`text-sm transition-colors duration-300 ${theme === "dark"
-                                    ? "text-slate-400"
-                                    : "text-slate-500"
+                                ? "text-slate-400"
+                                : "text-slate-500"
                                 }`}>
                                 Total Employees
                             </p>
@@ -99,12 +100,12 @@ function Employees() {
 
                     {/* Table */}
 
-                    <div className={`rounded-xl shadow-xl border overflow-hidden transition-colors duration-300 ${theme === "dark"
-                            ? "bg-slate-900 border-slate-700"
-                            : "bg-white border-slate-200"
+                    <div className={`w-full rounded-xl shadow-xl border overflow-hidden transition-colors duration-300 ${theme === "dark"
+                        ? "bg-slate-900 border-slate-700"
+                        : "bg-white border-slate-200"
                         }`}>
 
-                        <table className="w-full">
+                        <table className="w-full table-fixed">
 
                             <thead>
 
@@ -143,42 +144,42 @@ function Employees() {
                                         <tr
                                             key={employee._id}
                                             className={`border-b transition-all duration-300 ${theme === "dark"
-                                                    ? "border-slate-700 hover:bg-slate-800"
-                                                    : "border-slate-100 hover:bg-blue-50"
+                                                ? "border-slate-700 hover:bg-slate-800"
+                                                : "border-slate-100 hover:bg-blue-50"
                                                 }`}
                                         >
 
                                             <td className={`px-8 py-5 font-medium transition-colors duration-300 ${theme === "dark"
-                                                    ? "text-white"
-                                                    : "text-slate-700"
+                                                ? "text-white"
+                                                : "text-slate-700"
                                                 }`}>
                                                 {employee.employeeId}
                                             </td>
 
                                             <td className={`px-8 py-5 transition-colors duration-300 ${theme === "dark"
-                                                    ? "text-slate-200"
-                                                    : "text-slate-700"
+                                                ? "text-slate-200"
+                                                : "text-slate-700"
                                                 }`}>
                                                 {employee.fullName}
                                             </td>
 
                                             <td className={`px-8 py-5 transition-colors duration-300 ${theme === "dark"
-                                                    ? "text-slate-300"
-                                                    : "text-slate-600"
+                                                ? "text-slate-300"
+                                                : "text-slate-600"
                                                 }`}>
                                                 {employee.email}
                                             </td>
 
                                             <td className={`px-8 py-5 transition-colors duration-300 ${theme === "dark"
-                                                    ? "text-slate-300"
-                                                    : "text-slate-600"
+                                                ? "text-slate-300"
+                                                : "text-slate-600"
                                                 }`}>
                                                 {employee.department}
                                             </td>
 
                                             <td className={`px-8 py-5 transition-colors duration-300 ${theme === "dark"
-                                                    ? "text-slate-300"
-                                                    : "text-slate-600"
+                                                ? "text-slate-300"
+                                                : "text-slate-600"
                                                 }`}>
                                                 {employee.designation}
                                             </td>
@@ -194,8 +195,8 @@ function Employees() {
                                         <td
                                             colSpan="5"
                                             className={`py-16 text-center text-lg transition-colors duration-300 ${theme === "dark"
-                                                    ? "text-slate-400"
-                                                    : "text-slate-500"
+                                                ? "text-slate-400"
+                                                : "text-slate-500"
                                                 }`}
                                         >
                                             No employees found.
